49224ad7ea4a5c4322cc8f0a88529698a93d73c0
Cyril Alvarez <cyrilalavrezadriaansen@gmail.com>
2017-05-08 14:41:49 +0200

    add coin + coin icon to dashboard

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,2 +1,19 @@
 //drag and drop
-$(".sortable").sortable({revert: true});
+$(".sortable").sortable({ revert: true });
+
+$("#dropDownCoins").on("input", function () {
+    var coin = $(this).val();
+    if ($("#coins option").filter(function () {
+        return this.value === coin;
+    }).length) {
+        $.ajax({
+            url: "/send_save",
+            type: "POST",
+            dataType: "json",
+            data: {name: coin}
+        });
+        $("#asideCoins").append(
+            $(`<img src=/images/coins/${coin}.png>`),
+        )
+    }
+});
