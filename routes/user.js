8a3303a6e1e661db59c7f85b1701bcff928ef99f
Cyril Alvarez <cyrilalavrezadriaansen@gmail.com>
2017-05-08 08:26:00 +0200

    change the size of navbar icons and added all routes to Navbar

--- routes/user.js
+++ routes/user.js
@@ -11,4 +11,20 @@ router.get("/user/edit", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
   res.render('user/edit');
 });
 
+router.get("/user/portfolio", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+  res.render('user/portfolio');
+});
+
+router.get("/user/map", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+  res.render('user/map');
+});
+
+router.get("/user/email", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+  res.render('user/email');
+});
+
+router.get("/user/notifications", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+  res.render('user/notifications');
+});
+
 module.exports = router;
