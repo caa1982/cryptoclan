f9ae5a1b010d995c6a64fab652765879d453f4b2
Cyril Alvarez <cyrilalavrezadriaansen@gmail.com>
2017-05-08 13:21:24 +0200

    add dropdown menu coins on dashboard

--- routes/user.js
+++ routes/user.js
@@ -1,50 +1,55 @@
-const express     = require("express");
-const router      = express.Router();
+const express = require("express");
+const router = express.Router();
 const ensureLogin = require("connect-ensure-login");
-const User        = require("../models/user");
-const bcrypt     = require("bcrypt");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const Coin = require("../models/coin");
 const bcryptSalt = 10;
 
 
-router.get("/user/dashboard", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
-  res.render('user/dashboard');
+router.get("/user/dashboard", ensureLogin.ensureLoggedIn("/"), (req, res) => {
+  Coin.find({}, function (err, coins) {
+    res.render('user/dashboard', {
+      coins
+    });
+  });
 });
 
-router.get("/user/edit", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+router.get("/user/edit", ensureLogin.ensureLoggedIn("/"), (req, res) => {
   res.render('user/edit');
 });
 
 
-router.get("/user/portfolio", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+router.get("/user/portfolio", ensureLogin.ensureLoggedIn("/"), (req, res) => {
   res.render('user/portfolio');
 });
 
-router.get("/user/map", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+router.get("/user/map", ensureLogin.ensureLoggedIn("/"), (req, res) => {
   res.render('user/map');
 });
 
-router.get("/user/email", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+router.get("/user/email", ensureLogin.ensureLoggedIn("/"), (req, res) => {
   res.render('user/email');
 });
 
-router.get("/user/notifications", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+router.get("/user/notifications", ensureLogin.ensureLoggedIn("/"), (req, res) => {
   res.render('user/notifications');
 });
 
-router.get("/user/connect", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+router.get("/user/connect", ensureLogin.ensureLoggedIn("/"), (req, res) => {
   res.render('user/connect');
 });
 
-router.get("/user/logout", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+router.get("/user/logout", ensureLogin.ensureLoggedIn("/"), (req, res) => {
   req.logout();
   res.redirect('/');
 });
 
 
 
-router.post("/user/:userId", ensureLogin.ensureLoggedIn("/"), (req, res, next)=>{
+router.post("/user/:userId", ensureLogin.ensureLoggedIn("/"), (req, res, next) => {
   const password = req.body.password,
-        passwordRepeat = req.body.passwordRepeat;
+    passwordRepeat = req.body.passwordRepeat;
 
   const data = {
     name: req.body.name,
@@ -56,24 +61,24 @@ router.post("/user/:userId", ensureLogin.ensureLoggedIn("/"), (req, res, next)=>
     city: req.body.city
   }
 
-  if(password) {
-    if(password !== passwordRepeat) {
-      res.render("/user/edit", {message: "Passwords don't match"});
+  if (password) {
+    if (password !== passwordRepeat) {
+      res.render("/user/edit", { message: "Passwords don't match" });
       return;
     } else {
-      const salt     = bcrypt.genSaltSync(bcryptSalt);
+      const salt = bcrypt.genSaltSync(bcryptSalt);
       const hashPass = bcrypt.hashSync(password, salt);
       data.password = hashPass;
     }
   }
 
-  User.findOneAndUpdate({"_id":req.params.userId}, data, (err, user)=>{
-    if(err) {next(err); return;}
+  User.findOneAndUpdate({ "_id": req.params.userId }, data, (err, user) => {
+    if (err) { next(err); return; }
     res.redirect("/user/dashboard");
   })
 });
 
-router.get("/user/clan_join", ensureLogin.ensureLoggedIn("/"), (req, res)=>{
+router.get("/user/clan_join", ensureLogin.ensureLoggedIn("/"), (req, res) => {
   res.render('user/clan_join');
 });
 
