37d6eec8c4f4786c2cf8c74eb82d211e149dd121
Darek Glowinski <dariushg32111@gmail.com>
2017-05-11 19:53:53 +0200

    fix start in package.json

--- configuration.js
+++ configuration.js
@@ -11,5 +11,4 @@ module.exports = {
   coinmarketcapInterval: 5*60*1000,
   portfolioInterval: 30*1000
 }
-
-//mongo ds115131.mlab.com:15131/cryptoclan -u iamzim123 -p Sesame123
+  // "start": "nodemon --ignore 'public/javascripts/*.js' ./bin/www"
