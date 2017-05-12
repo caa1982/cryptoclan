ffc2005b48a4d0167f26bdd08eabea070bd47968
Darek Glowinski <dariushg32111@gmail.com>
2017-05-12 15:29:38 +0200

    fix coins added twice

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,16 +3,19 @@ $(document).ready(function () {
     $(".sortable").sortable({ revert: true });
 
     $("#dropDownCoins").on("input", function () {
+         console.log("here2")
         var coin = $(this).val();
         if ($("#dropDown option").filter(function () {
             return this.value === coin;
         }).length) {
+            console.log("here1")
             $.ajax({
                 url: "/send_save",
                 type: "POST",
                 data: { name: coin },
                 success: function (response) {
                     $("#dropDownCoins").val("");
+                    console.log("here")
                     $(".coinsDasboard").append(
                         $(`<img id="${coin}" src=https://files.coinmarketcap.com/static/img/coins/128x128/${coin}.png>`
                         ));
